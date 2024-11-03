d441f837094b76016d72176d29c258664be49e5a
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-11-03 16:55:14 -0300

    0.1

---
+++ android/app/src/main/kotlin/com/example/curso_flutterando_tv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.curso_flutterando_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Container(
+    child: Text('Fkuterrando', 
+    textDirection: TextDirection.rtl,
+    style: TextStyle(color: Colors.white, fontSize: 200.0),
+  )
+  ));
+}
