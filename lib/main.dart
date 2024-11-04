a074cdef4657348025681f7ac350d10e8fdae690
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-11-04 07:39:35 -0300

    0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 void main(List<String> args) {
-  runApp(Container(
-    child: Text('Fkuterrando', 
-    textDirection: TextDirection.rtl,
-    style: TextStyle(color: Colors.white, fontSize: 200.0),
-  )
-  ));
+  runApp(AppWidget());
 }
